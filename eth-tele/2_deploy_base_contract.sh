e4fed665488e531085b006ff542ce41dd5086848
wenbin <wenbin.shi@gmail.com>
2022-03-06 12:07:06 +0800

    fix build error

--- eth-tele/2_deploy_base_contract.sh
+++ eth-tele/2_deploy_base_contract.sh
@@ -9,30 +9,57 @@ function download_xibc() {
     echo "文件夹不存在"
     git clone git@github.com:teleport-network/xibc-contracts.git
     cd xibc-contracts
-    git checkout -b auto-deploy
-    cd evm && yarn && yarn compile && mv  hardhat.config.ts hardhat.config.bak.ts
-    cd ../erc20 && yarn && yarn compile && mv  hardhat.config.ts hardhat.config.bak.ts
+    git checkout  auto-deploy
+    cd evm && yarn && yarn compile
+    mv  hardhat.config.ts hardhat.config.bak.ts
+    cd ../erc20 && yarn && yarn compile
+    mv  hardhat.config.ts hardhat.config.bak.ts
 
     # exit to pre path
-    cd ../../  &&  echo $PWD
+    cd ../../../  &&  echo $PWD
 
     # replace config
     cp -r eth-tele/hardhat-config/hardhat.config.erc20.ts helper/xibc-contracts/erc20/hardhat.config.ts \
     && cp -r eth-tele/hardhat-config/hardhat.config.evm.ts helper/xibc-contracts/evm/hardhat.config.ts
 
+    # recompile for build error
+    cd helper/xibc-contracts/evm && yarn compile
+    cd ../erc20 && yarn compile
+    cd ../../
   else
     echo "文件夹存在"
   fi
 }
 
-function deploy_base_contract_on_eth(){
-  echo wenbin
-  source env_var/variable.sh
+function update_env(){
+  source ../eth-tele/env_var/variable.sh
 }
 
+function tool_check() {
+  while true; do
+  yarn hardhat deployLibraries --network $ETH_NETWORK_NAME
+  grep $1 env.txt
+  if [ "$?" = "0" ]; then
+    break
+  fi
+  done
+  echo "deploy $1 success!"
+  source env.txt
+}
+
+function deploy_base_on_eth() {
+    echo "wenbin"
+    cd xibc-contracts/evm
+
+    tool_check LIGHT_CLIENT_GEN_VALHASH_ADDRESS
+}
 
 download_xibc
 
-echo $PWD
+update_env
+
+deploy_base_on_eth
+
 
 # exit to pre path
+cd ../eth-tele
